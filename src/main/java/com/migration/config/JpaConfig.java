136818476d103270561a7ca715fd7d108b9ce090
fpomeisl <ferencpomeisl@gmail.com>
2019-12-16 15:06:07 +0100

    changes package address in annotation

--- src/main/java/com/migration/config/JpaConfig.java
+++ src/main/java/com/migration/config/JpaConfig.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableJpaRepositories(basePackages = {"com.liquibase.migration.example.repository"})
+@EnableJpaRepositories(basePackages = {"com.migration.repository"})
 @EnableTransactionManagement
 public class JpaConfig {
 }
